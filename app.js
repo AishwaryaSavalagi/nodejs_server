01800fdeb628e5ac306a9a576e48e93afa92c7b3
AishwaryaSavalagi <93003367+AishwaryaSavalagi@users.noreply.github.com>
2023-01-27 15:02:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '1234',
     database: 'Doctor_Appointment'
 });
 
